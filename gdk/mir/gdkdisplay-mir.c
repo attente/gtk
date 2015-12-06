d27e2c162945739df1a419f05d018f5afcb2a186
William Hua <william.hua@canonical.com>
2015-12-06 14:47:06 -0500

    mir: add empty backend

---
+++ gdk/mir/gdkdisplay-mir.c
@@ -0,0 +1,7 @@
+#include "gdkprivate-mir.h"
+
+GdkDisplay *
+_gdk_mir_display_open (const char *name)
+{
+  return NULL;
+}
---
+++ gdk/mir/gdkprivate-mir.h
@@ -0,0 +1,3 @@
+#include "gdktypes.h"
+
+GdkDisplay * _gdk_mir_display_open (const char *name);
